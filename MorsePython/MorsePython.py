3dc0e43ad3eb990af6a8092c5afe43474ba5ff60
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-16 10:27:56 +0200

    morseToLetter() and letterToMorse() implemented

--- MorsePython/MorsePython.py
+++ MorsePython/MorsePython.py
@@ -1,10 +1,23 @@
 import MorseTree
 from MorseTree import *
-import string
-from string import *
 
-def letterToMorse( letter ) :
-    return (morseTree[ord( letter ) - ord( "A" )][3])
+def letterToRank( letter ) :
+    return ord( letter ) - ord( "A" )
 
-letter = input()
-print( letterToMorse( letter ) )
+def rankToLetter( rank ) :
+    return chr( ord( "A" ) + rank )
+
+def letterToMorse( letterToCode ) :
+    return (morseTree[letterToRank( letterToCode )][3])
+
+def MorseToLetter( morseToDecode,  posTreeMorse = morseStart, posMorseToDecode = 0 ):
+    if (posMorseToDecode >= len( morseToDecode )) :
+        return morseTree[posTreeMorse][0]
+    if (morseToDecode[posMorseToDecode] == "."):
+        return MorseToLetter( morseToDecode, letterToRank( morseTree[posTreeMorse][1] ), posMorseToDecode + 1 )
+    elif (morseToDecode[posMorseToDecode] == "-"):
+        return MorseToLetter( morseToDecode, letterToRank( morseTree[posTreeMorse][2] ), posMorseToDecode + 1 )
+    return " "
+
+for i in range( 26 ):
+    print( str(rankToLetter(i)) + ":" +letterToMorse(rankToLetter(i)) +  ":" + MorseToLetter( letterToMorse( rankToLetter( i ) ) ) )
